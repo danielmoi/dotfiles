5ee4396707c75efee2494dead2f55d54d338bd3b
danielmoi <daniel@ampersandmoi.com>
2017-08-19 15:08:27 +1000

    Add public aliases

---
+++ .aliases.public.sh
@@ -0,0 +1,41 @@
+# APPS
+# 170505
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+alias code="/Applications/Visual\ Studio\ Code.app/Contents/MacOS/Electron"
+
+# DOCKER
+alias up="docker-compose up"
+
+# FILES - EDIT
+alias vimrc="vim ~/.dotfiles/.vimrc"
+alias bashp="vim ~/.dotfiles/.bash_profile"
+alias zshrc="vim ~/.dotfiles/.zshrc"
+
+# FILES/FOLDERS - VIEW
+# 170819
+alias lsl="ls -la | less"
+
+# FILES/FOLDERS - NAVIGATE
+
+# TERMINAL
+alias reload=". ~/.zshrc"
+
+# 161023
+alias echohome="echo this is my HOME: ${HOME}"
+alias echopath="echo this is my PATH: ${PATH}"
+
+
+# WEBPACK
+alias w="webpack"
+
+# YARN
+# 170216
+alias yb="yarn build"
+alias ybd="yarn build:dev"
+alias ys="yarn start"
+alias yl="yarn lint"
+# 170705
+alias yg="yarn go"
+alias ya="yarn assets"
+
+
